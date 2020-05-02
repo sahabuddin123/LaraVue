49120b9f9a05a70e5596b1828c423c980bfb65ec
sahabuddin <sahabuddinriyaj984@gmail.com>
2020-05-02 14:50:32 +0600

    Create App.ve and Update app.js => import bootstarp

---
+++ resources/js/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <main>
+        <Header />
+        <!-- END HEADER MOBILE-->
+        <!-- MENU SIDEBAR-->
+        <Sidebar />
+        <!-- END MENU SIDEBAR-->
+        <!-- PAGE CONTAINER-->
+        <div class="page-container">
+            <!-- HEADER DESKTOP-->
+            <HeaderRight />
+            <!-- END HEADER DESKTOP-->
+            <!-- MAIN CONTENT-->
+            <div class="main-content">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+                        <router-view></router-view>
+                        <Footer />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <FlashMessage position="right bottom"></FlashMessage>
+    </main>
+</template>
+<script>
+import Header from "./components/Header.vue";
+import Sidebar from "./components/Sidebar.vue";
+import HeaderRight from "./components/HeaderRight.vue";
+import Footer from "./components/Footer.vue";
+export default {
+    components: {
+        Header,
+        Sidebar,
+        HeaderRight,
+        Footer
+    }
+};
+</script>
--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-md-12">
             <div class="copyright">
-                <p>Copyright © 2020 SAtech360. All rights reserved. Template by <a href="http://satech360.ebookstand.net">Ebookstand</a></p>
+                <p>Copyright © 2020 SAtech360. All rights reserved. Template by <a href="http://satech360.ebookstand.net">SAtech360</a></p>
             </div>
         </div>
     </div>
