41f0464306a1a7ce0dff8f7e77a1fe01de90371f
sahabuddin <sahabuddinriyaj984@gmail.com>
2020-05-02 15:17:28 +0600

    Update Dashboard.vue

--- public/js/app.js
+++ public/js/app.js
@@ -19901,6 +19901,119 @@ render._withStripped = true
 
 /***/ }),
 
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/view/Dashboard.vue?vue&type=template&id=7e47a088&":
+/*!******************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/view/Dashboard.vue?vue&type=template&id=7e47a088& ***!
+  \******************************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _vm._m(0)
+}
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", [
+      _c("div", { staticClass: "row m-t-25" }, [
+        _c("div", { staticClass: "col-sm-6 col-lg-3" }, [
+          _c("div", { staticClass: "overview-item overview-item--c1" }, [
+            _c("div", { staticClass: "overview__inner" }, [
+              _c("div", { staticClass: "overview-box clearfix" }, [
+                _c("div", { staticClass: "icon" }, [
+                  _c("i", { staticClass: "zmdi zmdi-account-o" })
+                ]),
+                _vm._v(" "),
+                _c("a", { attrs: { href: "" } }, [
+                  _c("div", { staticClass: "text" }, [
+                    _c("h2"),
+                    _vm._v(" "),
+                    _c("span", [_vm._v("Total ")])
+                  ])
+                ])
+              ])
+            ])
+          ])
+        ]),
+        _vm._v(" "),
+        _c("div", { staticClass: "col-sm-6 col-lg-3" }, [
+          _c("div", { staticClass: "overview-item overview-item--c2" }, [
+            _c("div", { staticClass: "overview__inner" }, [
+              _c("div", { staticClass: "overview-box clearfix" }, [
+                _c("div", { staticClass: "icon" }, [
+                  _c("i", { staticClass: "zmdi zmdi-account" })
+                ]),
+                _vm._v(" "),
+                _c("a", { attrs: { href: "" } }, [
+                  _c("div", { staticClass: "text" }, [
+                    _c("h2"),
+                    _vm._v(" "),
+                    _c("span", [_vm._v("Active ")])
+                  ])
+                ])
+              ])
+            ])
+          ])
+        ]),
+        _vm._v(" "),
+        _c("div", { staticClass: "col-sm-6 col-lg-3" }, [
+          _c("div", { staticClass: "overview-item overview-item--c3" }, [
+            _c("div", { staticClass: "overview__inner" }, [
+              _c("div", { staticClass: "overview-box clearfix" }, [
+                _c("div", { staticClass: "icon" }, [
+                  _c("i", { staticClass: "zmdi zmdi-account" })
+                ]),
+                _vm._v(" "),
+                _c("a", { attrs: { href: "" } }, [
+                  _c("div", { staticClass: "text" }, [
+                    _c("h2"),
+                    _vm._v(" "),
+                    _c("span", [_vm._v("Inactive ")])
+                  ])
+                ])
+              ])
+            ])
+          ])
+        ]),
+        _vm._v(" "),
+        _c("div", { staticClass: "col-sm-6 col-lg-3" }, [
+          _c("div", { staticClass: "overview-item overview-item--c4" }, [
+            _c("div", { staticClass: "overview__inner" }, [
+              _c("div", { staticClass: "overview-box clearfix" }, [
+                _c("div", { staticClass: "icon" }, [
+                  _c("i", { staticClass: "zmdi zmdi-link" })
+                ]),
+                _vm._v(" "),
+                _c("a", { attrs: { href: "" } }, [
+                  _c("div", { staticClass: "text" }, [
+                    _c("h2"),
+                    _vm._v(" "),
+                    _c("span", [_vm._v("Total Link")])
+                  ])
+                ])
+              ])
+            ])
+          ])
+        ])
+      ])
+    ])
+  }
+]
+render._withStripped = true
+
+
+
+/***/ }),
+
 /***/ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js":
 /*!********************************************************************!*\
   !*** ./node_modules/vue-loader/lib/runtime/componentNormalizer.js ***!
@@ -35346,17 +35459,18 @@ component.options.__file = "resources/js/view/Categories.vue"
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
-var render, staticRenderFns
+/* harmony import */ var _Dashboard_vue_vue_type_template_id_7e47a088___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Dashboard.vue?vue&type=template&id=7e47a088& */ "./resources/js/view/Dashboard.vue?vue&type=template&id=7e47a088&");
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
 var script = {}
 
 
 /* normalize component */
 
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_0__["default"])(
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_1__["default"])(
   script,
-  render,
-  staticRenderFns,
+  _Dashboard_vue_vue_type_template_id_7e47a088___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _Dashboard_vue_vue_type_template_id_7e47a088___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
   false,
   null,
   null,
@@ -35364,8 +35478,28 @@ var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_
   
 )
 
+/* hot reload */
+if (false) { var api; }
 component.options.__file = "resources/js/view/Dashboard.vue"
 /* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/view/Dashboard.vue?vue&type=template&id=7e47a088&":
+/*!************************************************************************!*\
+  !*** ./resources/js/view/Dashboard.vue?vue&type=template&id=7e47a088& ***!
+  \************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Dashboard_vue_vue_type_template_id_7e47a088___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../node_modules/vue-loader/lib??vue-loader-options!./Dashboard.vue?vue&type=template&id=7e47a088& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/view/Dashboard.vue?vue&type=template&id=7e47a088&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Dashboard_vue_vue_type_template_id_7e47a088___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Dashboard_vue_vue_type_template_id_7e47a088___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+
+
 
 /***/ }),
 
---
+++ resources/js/view/Dashboard.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <div class="row m-t-25">
+            <div class="col-sm-6 col-lg-3">
+                <div class="overview-item overview-item--c1">
+                    <div class="overview__inner">
+                        <div class="overview-box clearfix">
+                            <div class="icon">
+                                <i class="zmdi zmdi-account-o"></i>
+                            </div>
+                            <a href="">
+                                <div class="text">
+                                    <h2></h2>
+                                    <span>Total </span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="overview-item overview-item--c2">
+                    <div class="overview__inner">
+                        <div class="overview-box clearfix">
+                            <div class="icon">
+                                <i class="zmdi zmdi-account"></i>
+                            </div>
+                            <a href="">
+                                <div class="text">
+                                    <h2></h2>
+                                    <span>Active </span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="overview-item overview-item--c3">
+                    <div class="overview__inner">
+                        <div class="overview-box clearfix">
+                            <div class="icon">
+                                <i class="zmdi zmdi-account"></i>
+                            </div>
+                            <a href="">
+                                <div class="text">
+                                    <h2></h2>
+                                    <span>Inactive </span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="overview-item overview-item--c4">
+                    <div class="overview__inner">
+                        <div class="overview-box clearfix">
+                            <div class="icon">
+                                <i class="zmdi zmdi-link"></i>
+                            </div>
+                            <a href="">
+                                <div class="text">
+                                    <h2></h2>
+                                    <span>Total Link</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
