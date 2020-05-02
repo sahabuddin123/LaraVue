facf398c2111731dd2253e717567da261a407af8
sahabuddin <sahabuddinriyaj984@gmail.com>
2020-05-02 15:49:05 +0600

    install vuex and bootstaro-vue and create store.js and update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,19 @@
 import Vue from 'vue';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import App from './App.vue';
 import router from './router';
+import store from './store';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'bootstrap';
+
+
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 new Vue({
     el: '#app',
     router,
+    store,
     render: h => h(App)
 });
