9c7261cc4d32b4be7c7aa39c30542b43771e6c90
sahabuddin <sahabuddinriyaj984@gmail.com>
2020-05-03 16:25:21 +0600

    update kernel.php and test administrator and author api

--- routes/api.php
+++ routes/api.php
@@ -10,4 +10,22 @@ Route::group(['prefix' => 'auth'], function () {
         Route::get('logout', 'AuthController@logout');
     });
 });
+
+Route::group(['prefix' => 'user'], function () {
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::post('edit-category', function () {
+            return response()->json([
+                'message' => 'Admin Access!',
+                'status_code' => 200
+            ], 200);
+        })->middleware('scope:do_anything');
+        //
+        Route::post('create-category', function () {
+            return response()->json([
+                'message' => 'Everyone Access!',
+                'status_code' => 200
+            ], 200);
+        })->middleware('scope:do_anything,can_create');
+    });
+});
 Route::resource('categories', 'CategoryController');
